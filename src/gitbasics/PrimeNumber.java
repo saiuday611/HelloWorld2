fe76ebddb5424d1e993cd41d3355315e4130f9c1
sai Uday <saiuday611@gmail.com>
2026-03-03 20:36:52 +0530

    Second commit - Prime code

---
+++ src/gitbasics/PrimeNumber.java
@@ -0,0 +1,27 @@
+package gitbasics;
+import java.util.*;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int n=sc.nextInt();
+		boolean isPrime=true;
+		for(int i=2;i<n;i++) {
+			if(n%i==0) {
+				isPrime=false;
+				break;
+			}
+		}
+		if(isPrime) {
+			System.out.println("Prime");
+		}
+		else {
+			System.out.println("Not a prime");
+		}
+
+	}
+
+}
